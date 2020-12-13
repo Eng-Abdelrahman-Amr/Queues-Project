6a7ceb2ed21c2293e7625dbeb75df4d0952eff5e
Eng-Abdelrahman-Amr <75952980+Eng-Abdelrahman-Amr@users.noreply.github.com>
2020-12-13 23:06:34 +0200

    Add files via upload

---
+++ QueuProject.java
@@ -0,0 +1,15 @@
+
+package queuproject;
+
+public class QueuProject {
+
+    public static void main(String[] args) {
+        Queue_Frame qf=new Queue_Frame();
+        qf.setVisible(true);
+        qf.setTitle("Queues Project");
+        qf.setLocationRelativeTo(null);
+    
+       
+    }
+    
+}
